e8b35cb4f21c6b4822b1e8e3c673648cad5bcb36
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 00:42:33 +0700

    feat: create a view to select menu and add it to cart

---
+++ internal/menu/main.go
@@ -0,0 +1,77 @@
+package menu
+
+import (
+	"bufio"
+	"fmt"
+	"golang-weekly/internal/cart"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func SelectMenu() {
+	loop := true
+	reader := bufio.NewReader(os.Stdin)
+	for loop {
+		fmt.Printf("\x1bc")
+		fmt.Print("--- Select Menu ---\n\n")
+
+		for _, item := range Menus {
+			fmt.Printf("%d. %s - Rp %.2f\n", item.ID, item.Name, item.Price)
+		}
+		fmt.Print("\n0. exit\n\n")
+
+		fmt.Print("\nChoose a menu: ")
+
+		choiceStr, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Invalid input, please enter a number.")
+			continue
+		}
+
+		choiceStr = strings.TrimSpace(choiceStr)
+		choice, err := strconv.Atoi(choiceStr)
+		if err != nil {
+			fmt.Println("Invalid input, please enter a number.")
+			continue
+		}
+
+		if choice == 0 {
+			loop = false
+			continue
+		}
+
+		found := false
+		for _, item := range Menus {
+			if item.ID == choice {
+				fmt.Println("You selected:", item.Name)
+				found = true
+				itemExists := false
+				for i := range cart.Carts {
+					if cart.Carts[i].ID == item.ID {
+						cart.Carts[i].Quantity++
+						itemExists = true
+						break
+					}
+				}
+
+				if !itemExists {
+					cart.Carts = append(cart.Carts, cart.CartItem{
+						ID:       item.ID,
+						Name:     item.Name,
+						Quantity: 1,
+						Price:    item.Price,
+					})
+				}
+				fmt.Printf("%s has been added to your cart.\n", item.Name)
+			}
+		}
+
+		if !found {
+			fmt.Println("Menu item not found, please try again.")
+		}
+
+		fmt.Print("\nPress Enter to continue...")
+		fmt.Scanln()
+	}
+}
