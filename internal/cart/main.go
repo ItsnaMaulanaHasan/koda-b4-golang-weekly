c235062fe091e808ed113178f42c50e46c9c6e87
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 00:42:56 +0700

    feat: create a view to view the cart list

---
+++ internal/cart/main.go
@@ -0,0 +1,30 @@
+package cart
+
+import "fmt"
+
+func ShowCart() {
+	loop := true
+	for loop {
+		fmt.Println("\x1bc")
+		println("--- Your Cart ---\n")
+
+		if len(Carts) == 0 {
+			fmt.Println("Your cart is empty.")
+		} else {
+			total := 0.0
+			for _, item := range Carts {
+				subtotal := float64(item.Quantity) * item.Price
+				fmt.Println(item.Name, "- Quantity:", item.Quantity, "- Subtotal: Rp", subtotal)
+				total += subtotal
+			}
+			fmt.Println("\nTotal: Rp", total)
+		}
+
+		fmt.Print("\nPress 0 to go back to the main menu ")
+		var input string
+		fmt.Scanln(&input)
+		if input == "0" {
+			loop = false
+		}
+	}
+}
