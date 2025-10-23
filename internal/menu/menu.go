3236400e5f41abc080da914351683a8c892fc43b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 00:40:41 +0700

    feat: create a menu data list and menu data on the home page

---
+++ internal/menu/menu.go
@@ -0,0 +1,38 @@
+package menu
+
+import "golang-weekly/internal/cart"
+
+type MenuItem struct {
+	ID    int
+	Name  string
+	Price float64
+}
+
+type MenuHome struct {
+	ID     int
+	Menu   string
+	Action func()
+}
+
+var Menus = []MenuItem{
+	{ID: 1, Name: "Mixue Ice Cream", Price: 8000},
+	{ID: 2, Name: "BOBA Sundae", Price: 16000},
+	{ID: 3, Name: "Strawberry Mi-Shake", Price: 16000},
+	{ID: 4, Name: "BOBA Mi-Shake", Price: 16000},
+	{ID: 5, Name: "Chocolate Cookies Smoothies", Price: 16000},
+	{ID: 6, Name: "Brown Sugar Pearl Milk Tea", Price: 19000},
+	{ID: 7, Name: "Pearl Milk Tea", Price: 22000},
+	{ID: 8, Name: "Oats Milk Tea", Price: 22000},
+	{ID: 9, Name: "Coconut Jelly Milk Tea", Price: 22000},
+	{ID: 10, Name: "Red Bean Milk Tea", Price: 22000},
+	{ID: 11, Name: "Fresh Squeezed Lemonade", Price: 10000},
+	{ID: 12, Name: "Peach Earl Grey Tea", Price: 16000},
+	{ID: 13, Name: "Original Jasmine Tea", Price: 10000},
+	{ID: 14, Name: "Original Earl Grey Tea", Price: 10000},
+}
+
+var MenuHomes = []MenuHome{
+	{ID: 1, Menu: "Select Menu", Action: SelectMenu},
+	{ID: 2, Menu: "Cart", Action: cart.ShowCart},
+	{ID: 3, Menu: "History", Action: nil},
+}
