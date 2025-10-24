d655136a36e6597a7d5d6c7f2a25c62c54d6a69f
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 20:55:51 +0700

    feat: create function to clear cart

--- internal/cart/cart.go
+++ internal/cart/cart.go
@@ -14,5 +14,5 @@ var Carts []CartItem
 var CartMenus = []datamenu.MenusPage{
 	{ID: 1, Menu: "Checkout", Action: CheckoutCart},
 	{ID: 2, Menu: "Edit Cart", Action: nil},
-	{ID: 3, Menu: "Clear Cart", Action: nil},
+	{ID: 3, Menu: "Clear Cart", Action: ClearCart},
 }
---
+++ internal/cart/clearcarts.go
@@ -0,0 +1,31 @@
+package cart
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ClearCart() {
+	loop := true
+	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
+	for loop {
+		fmt.Println("\x1bc")
+		fmt.Print("Are you sure you want to clear carts (y/n)? ")
+		choiceStr, _ := reader.ReadString('\n')
+		choiceStr = strings.TrimSpace(choiceStr)
+		if strings.ToLower(choiceStr) == "y" {
+			Carts = []CartItem{}
+			fmt.Print("Carts successfully cleared! Press enter to continue... ")
+			scanner.Scan()
+			loop = false
+		} else if strings.ToLower(choiceStr) == "n" {
+			loop = false
+		} else {
+			fmt.Print("Invalid input, please enter 'y' or 'n'... ")
+			scanner.Scan()
+		}
+	}
+}
