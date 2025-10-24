ec8cfba6f6d85bdf16c72cf9e38715585fd1006d
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-25 01:59:37 +0700

    feat: adjust func show histories

--- internal/history/show_histories.go
+++ internal/history/show_histories.go
@@ -3,6 +3,7 @@ package history
 import (
 	"bufio"
 	"fmt"
+	"golang-weekly/internal/models"
 	"os"
 	"strconv"
 	"strings"
@@ -12,25 +13,27 @@ func ShowHistories() {
 	loop := true
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for loop {
 		fmt.Println("\x1bc")
 		fmt.Print("--- Your Histories ---\n\n")
 
-		if len(Histories) == 0 {
-			fmt.Print("Your cart is empty.\n\n")
+		if len(models.Histories) == 0 {
+			fmt.Print("Your histories is empty.\n\n")
 			fmt.Print("-----------------\n\n")
-			fmt.Print("Enter to go back to the main menu... ")
+			fmt.Print("Press Enter to go back to the main menu... ")
 			scanner.Scan()
 			loop = false
 			continue
 		}
 
-		for i, history := range Histories {
-			fmt.Printf("%d. ID: %d - Date: %s - No Invoice: %s - Total: Rp %d\n", i+1, history.ID, history.Date, history.NoInvoice, history.Total)
+		for i, history := range models.Histories {
+			fmt.Printf("%d. ID: %d - Date: %s - No Invoice: %s - Total: Rp %.2f\n",
+				i+1, history.ID, history.Date, history.NoInvoice, history.Total)
 		}
 		fmt.Print("\n-----------------\n\n")
-		fmt.Print("\n0. exit\n")
-		fmt.Println("Enter number to view details: ")
+		fmt.Print("0. exit\n\n")
+		fmt.Print("Enter number to view details: ")
 
 		choiceStr, _ := reader.ReadString('\n')
 		choiceStr = strings.TrimSpace(choiceStr)
@@ -47,25 +50,6 @@ func ShowHistories() {
 			break
 		}
 
-		found := false
-		for _, history := range Histories {
-			if choice == history.ID {
-				fmt.Println("=== History Details ===")
-				fmt.Println("ID:", history.ID)
-				fmt.Println("Date:", history.Date)
-				fmt.Println("No Invoice:", history.NoInvoice)
-				fmt.Println("Menu Items:")
-				for _, menu := range history.ListMenu {
-					fmt.Println("- ", menu.Name, "Price:", menu.Price)
-				}
-				fmt.Println("Total:", history.Total)
-				found = true
-				break
-			}
-		}
-		if !found {
-			fmt.Println("History ID not found, press enter to continue...")
-			scanner.Scan()
-		}
+		DetailsHistory(choice - 1)
 	}
 }
