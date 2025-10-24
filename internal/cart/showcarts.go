df60d5b9eb669b25c6475cccf96970dc9e518c45
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 18:03:28 +0700

    feat: added validation input and adjust view in showcarts

--- internal/cart/showcarts.go
+++ internal/cart/showcarts.go
@@ -1,17 +1,27 @@
 package cart
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func ShowCarts() {
 	loop := true
+	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	for loop {
 		fmt.Println("\x1bc")
-		fmt.Print("--- Your Cart ---\n")
+		fmt.Print("--- Your Cart ---\n\n")
 
 		if len(Carts) == 0 {
-			fmt.Println("Your cart is empty.")
+			fmt.Print("Your cart is empty.\n\n")
+			fmt.Print("-----------------\n\n")
+			fmt.Print("Enter to go back to the main menu... ")
+			scanner.Scan()
+			loop = false
 		} else {
 			total := 0.0
 			for _, item := range Carts {
@@ -19,18 +29,51 @@ func ShowCarts() {
 				fmt.Println(item.Name, "- Quantity:", item.Quantity, "- Subtotal: Rp", subtotal)
 				total += subtotal
 			}
+
 			fmt.Println("\nTotal: Rp", total)
-		}
+			fmt.Print("\n-----------------\n\n")
 
-		for _, menu := range CartMenus {
-			fmt.Printf("%d. %s\n", menu.ID, menu.Menu)
-		}
+			for _, menu := range CartMenus {
+				fmt.Printf("%d. %s\n", menu.ID, menu.Menu)
+			}
 
-		fmt.Print("\nPress 0 to go back to the main menu ")
-		var input string
-		fmt.Scanln(&input)
-		if input == "0" {
-			loop = false
+			fmt.Print("\n0. exit\n\n")
+
+			fmt.Print("\nChoose a menu: ")
+
+			choiceStr, _ := reader.ReadString('\n')
+			choiceStr = strings.TrimSpace(choiceStr)
+			choice, err := strconv.Atoi(choiceStr)
+
+			if err != nil {
+				fmt.Print("Invalid input, please enter a number... ")
+				scanner.Scan()
+				continue
+			}
+
+			if choice == 0 {
+				loop = false
+				break
+			}
+
+			found := false
+			for _, menu := range CartMenus {
+				if menu.ID == choice {
+					if menu.Action != nil {
+						menu.Action()
+					} else {
+						fmt.Print("Menu action not implemented yet...")
+						scanner.Scan()
+					}
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				fmt.Print("Invalid menu option, press enter to continue...")
+				scanner.Scan()
+			}
 		}
 	}
 }
