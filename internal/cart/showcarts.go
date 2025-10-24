d77d740fc4cd7a6da33266db4851f1672175e5cb
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-24 20:56:25 +0700

    feat: create own file for checkoutcart function

--- internal/cart/showcarts.go
+++ internal/cart/showcarts.go
@@ -8,29 +8,6 @@ import (
 	"strings"
 )
 
-func CheckoutCart() {
-	loop := true
-	reader := bufio.NewReader(os.Stdin)
-	scanner := bufio.NewScanner(os.Stdin)
-	for loop {
-		fmt.Println("\x1bc")
-		fmt.Print("Are you sure you want to checkout (y/n)? ")
-		choiceStr, _ := reader.ReadString('\n')
-		choiceStr = strings.TrimSpace(choiceStr)
-		if strings.ToLower(choiceStr) == "y" {
-			Carts = []CartItem{}
-			fmt.Print("Checkout successful! Press enter to continue...")
-			scanner.Scan()
-			loop = false
-		} else if strings.ToLower(choiceStr) == "n" {
-			loop = false
-		} else {
-			fmt.Print("Invalid input, please enter 'y' or 'n'... ")
-			scanner.Scan()
-		}
-	}
-}
-
 func ShowCarts() {
 	loop := true
 	reader := bufio.NewReader(os.Stdin)
