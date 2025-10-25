5b4d454f4e2a2f0f384badf4611de7316c4f5506
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-25 19:10:28 +0700

    feat: apply panic and recover on function clear_carts

--- internal/cart/clear_carts.go
+++ internal/cart/clear_carts.go
@@ -13,20 +13,29 @@ func ClearCart() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
 	for loop {
-		fmt.Println("\x1bc")
-		fmt.Print("Are you sure you want to clear carts (y/n)? ")
-		choiceStr, _ := reader.ReadString('\n')
-		choiceStr = strings.TrimSpace(choiceStr)
-		if strings.ToLower(choiceStr) == "y" {
-			models.Carts = []models.CartItem{}
-			fmt.Print("Carts successfully cleared! Press enter to continue... ")
-			scanner.Scan()
-			loop = false
-		} else if strings.ToLower(choiceStr) == "n" {
-			loop = false
-		} else {
-			fmt.Print("Invalid input, please enter 'y' or 'n'... ")
-			scanner.Scan()
-		}
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Print(r)
+					scanner.Scan()
+				}
+			}()
+			fmt.Println("\x1bc")
+			fmt.Print("Are you sure you want to clear carts (y/n)? ")
+			choiceStr, _ := reader.ReadString('\n')
+			choiceStr = strings.TrimSpace(choiceStr)
+			if strings.ToLower(choiceStr) == "y" {
+				models.Carts = []models.CartItem{}
+				fmt.Print("Carts successfully cleared! Press enter to continue... ")
+				scanner.Scan()
+				loop = false
+				return
+			} else if strings.ToLower(choiceStr) == "n" {
+				loop = false
+				return
+			} else {
+				panic("Invalid input, please enter 'y' or 'n'... ")
+			}
+		}()
 	}
 }
