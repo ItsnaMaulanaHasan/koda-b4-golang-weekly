21c97f92c27c54051067c77173e6c0158ff81e5a
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-25 19:09:08 +0700

    feat: apply panic and recover on function edit_cart

--- internal/cart/edit_cart.go
+++ internal/cart/edit_cart.go
@@ -14,53 +14,58 @@ func EditCart() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
 	for loop {
-		fmt.Println("\x1bc")
-		fmt.Print("----------------- Your Carts -----------------------\n\n")
-
-		showCarts()
-
-		fmt.Print("0. Exit\n")
-
-		fmt.Print("\nEnter the menu number you want to edit: ")
-		choiceStr, _ := reader.ReadString('\n')
-		choiceStr = strings.TrimSpace(choiceStr)
-		choice, err := strconv.Atoi(choiceStr)
-
-		if err != nil {
-			fmt.Print("Invalid input, please enter a number... ")
-			scanner.Scan()
-			continue
-		}
-
-		if choice == 0 {
-			loop = false
-			break
-		}
-
-		found := false
-		for i, item := range models.Carts {
-			if item.ID == choice {
-				fmt.Printf("Enter new quantity for %s: ", item.Name)
-				quantityStr, _ := reader.ReadString('\n')
-				quantityStr = strings.TrimSpace(quantityStr)
-				quantity, err := strconv.Atoi(quantityStr)
-				if err != nil || quantity < 0 {
-					fmt.Print("Invalid quantity, please enter a valid number... ")
-				} else if quantity == 0 {
-					models.Carts = append(models.Carts[:i], models.Carts[i+1:]...)
-					fmt.Printf("%s removed from cart. Press enter to continue... ", item.Name)
-				} else {
-					models.Carts[i].Quantity = quantity
-					fmt.Printf("Quantity for %s updated to %d. Press enter to continue... ", item.Name, quantity)
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Print(r)
+					scanner.Scan()
 				}
-				scanner.Scan()
-				found = true
-				break
+			}()
+			fmt.Println("\x1bc")
+			fmt.Print("----------------- Your Carts -----------------------\n\n")
+
+			showCarts()
+
+			fmt.Print("0. Exit\n")
+
+			fmt.Print("\nEnter the menu number you want to edit: ")
+			choiceStr, _ := reader.ReadString('\n')
+			choiceStr = strings.TrimSpace(choiceStr)
+			choice, err := strconv.Atoi(choiceStr)
+
+			if err != nil {
+				panic("Invalid input, please enter a number... ")
 			}
-		}
-		if !found {
-			fmt.Print("Invalid menu option, press enter to continue...")
-			scanner.Scan()
-		}
+
+			if choice == 0 {
+				loop = false
+				return
+			}
+
+			found := false
+			for i, item := range models.Carts {
+				if item.ID == choice {
+					fmt.Printf("Enter new quantity for %s: ", item.Name)
+					quantityStr, _ := reader.ReadString('\n')
+					quantityStr = strings.TrimSpace(quantityStr)
+					quantity, err := strconv.Atoi(quantityStr)
+					if err != nil || quantity < 0 {
+						panic("Invalid quantity, please enter a valid number... ")
+					} else if quantity == 0 {
+						models.Carts = append(models.Carts[:i], models.Carts[i+1:]...)
+						fmt.Printf("%s removed from cart. Press enter to continue... ", item.Name)
+					} else {
+						models.Carts[i].Quantity = quantity
+						fmt.Printf("Quantity for %s updated to %d. Press enter to continue... ", item.Name, quantity)
+					}
+					scanner.Scan()
+					found = true
+					break
+				}
+			}
+			if !found {
+				panic("Invalid menu option, press enter to continue...")
+			}
+		}()
 	}
 }
