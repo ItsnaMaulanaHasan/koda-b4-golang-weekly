29c035ebe6870d3b8fb055d56b053e22f64d14b7
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-25 19:07:24 +0700

    feat: apply panic and recover on function show_carts

--- internal/cart/show_carts.go
+++ internal/cart/show_carts.go
@@ -41,61 +41,64 @@ func CartsPage() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
 	for loop {
-		fmt.Println("\x1bc")
-		fmt.Print("----------------- Your Carts -----------------------\n\n")
-
-		if len(models.Carts) == 0 {
-			fmt.Print("Your carts is empty.\n\n")
-			fmt.Print("----------------------------------------------------\n\n")
-			fmt.Print("Press enter to go back to the main menu... ")
-			scanner.Scan()
-			loop = false
-			continue
-		}
-
-		showCarts()
-
-		for _, menu := range CartMenus {
-			fmt.Printf("%d. %s\n", menu.ID, menu.Menu)
-		}
-
-		fmt.Print("\n0. Exit\n")
-
-		fmt.Print("\nChoose a menu: ")
-
-		choiceStr, _ := reader.ReadString('\n')
-		choiceStr = strings.TrimSpace(choiceStr)
-		choice, err := strconv.Atoi(choiceStr)
-
-		if err != nil {
-			fmt.Print("Invalid input, please enter a number... ")
-			scanner.Scan()
-			continue
-		}
-
-		if choice == 0 {
-			loop = false
-			break
-		}
-
-		found := false
-		for _, menu := range CartMenus {
-			if menu.ID == choice {
-				if menu.Action != nil {
-					menu.Action()
-				} else {
-					fmt.Print("Menu action not implemented yet...")
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Print(r)
 					scanner.Scan()
 				}
-				found = true
-				break
+			}()
+			fmt.Println("\x1bc")
+			fmt.Print("----------------- Your Carts -----------------------\n\n")
+
+			if len(models.Carts) == 0 {
+				fmt.Print("Your carts is empty.\n\n")
+				fmt.Print("----------------------------------------------------\n\n")
+				fmt.Print("Press enter to go back to the main menu... ")
+				scanner.Scan()
+				loop = false
+				return
 			}
-		}
 
-		if !found {
-			fmt.Print("Invalid menu option, press enter to continue...")
-			scanner.Scan()
-		}
+			showCarts()
 
+			for _, menu := range CartMenus {
+				fmt.Printf("%d. %s\n", menu.ID, menu.Menu)
+			}
+
+			fmt.Print("\n0. Exit\n")
+
+			fmt.Print("\nChoose a menu: ")
+
+			choiceStr, _ := reader.ReadString('\n')
+			choiceStr = strings.TrimSpace(choiceStr)
+			choice, err := strconv.Atoi(choiceStr)
+
+			if err != nil {
+				panic("Invalid input, please enter a number... ")
+			}
+
+			if choice == 0 {
+				loop = false
+				return
+			}
+
+			found := false
+			for _, menu := range CartMenus {
+				if menu.ID == choice {
+					if menu.Action != nil {
+						menu.Action()
+					} else {
+						panic("Menu action not implemented yet...")
+					}
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				panic("Invalid menu option, press enter to continue...")
+			}
+		}()
 	}
 }
