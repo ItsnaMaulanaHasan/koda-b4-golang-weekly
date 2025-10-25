fdaf40cf638940c4a33f283c6d7e9dc50035c4e5
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-25 19:09:40 +0700

    feat: apply panic and recover on function checkout_carts

--- internal/cart/checkout_carts.go
+++ internal/cart/checkout_carts.go
@@ -13,21 +13,30 @@ func CheckoutCart() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(os.Stdin)
 	for loop {
-		fmt.Println("\x1bc")
-		fmt.Print("Are you sure you want to checkout (y/n)? ")
-		choiceStr, _ := reader.ReadString('\n')
-		choiceStr = strings.TrimSpace(choiceStr)
-		if strings.ToLower(choiceStr) == "y" {
-			CreateInvoice(models.Carts)
-			models.Carts = []models.CartItem{}
-			fmt.Print("Checkout successful! Press enter to continue... ")
-			scanner.Scan()
-			loop = false
-		} else if strings.ToLower(choiceStr) == "n" {
-			loop = false
-		} else {
-			fmt.Print("Invalid input, please enter 'y' or 'n'... ")
-			scanner.Scan()
-		}
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Print(r)
+					scanner.Scan()
+				}
+			}()
+			fmt.Println("\x1bc")
+			fmt.Print("Are you sure you want to checkout (y/n)? ")
+			choiceStr, _ := reader.ReadString('\n')
+			choiceStr = strings.TrimSpace(choiceStr)
+			if strings.ToLower(choiceStr) == "y" {
+				CreateInvoice(models.Carts)
+				models.Carts = []models.CartItem{}
+				fmt.Print("Checkout successful! Press enter to continue... ")
+				scanner.Scan()
+				loop = false
+				return
+			} else if strings.ToLower(choiceStr) == "n" {
+				loop = false
+				return
+			} else {
+				panic("Invalid input, please enter 'y' or 'n'... ")
+			}
+		}()
 	}
 }
